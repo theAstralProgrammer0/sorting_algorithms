232c4bcb7754c3933ad6d257ed0244ffadf12c87
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-16 18:52:51 +0100

    Added changes

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,16 @@
 #include "sort.h"
 
+/**
+ * swap - Aux function
+ *
+ * Description: This is a function that swaps two integer values
+ *
+ * @a: fisrt integer
+ *
+ * @b: second integer
+ *
+ * Return: Nothing
+ */
 void swap(int *a, int *b)
 {
         int temp;
@@ -9,28 +20,23 @@ void swap(int *a, int *b)
         *b = temp;
 }
 
-void sort(int *array, int lb, int ub, size_t size)
-{
-	int loc;
 
-	if (lb >= ub || lb < 0)
-		return;
-
-	loc = Partition(array, lb, ub, size);
-	sort(array, lb, loc - 1, size);
-	sort(array, loc + 1, ub, size);
-}
-
-void quick_sort(int *array, size_t size)
-{
-	int lb, ub;
-
-	lb = 0;
-	ub = size - 1;
-	sort(array, lb, ub, size);
-}
-
-
+/**
+ * Partition - Aux function
+ *
+ * Description: This is a function that partitions into two based on a pivot
+ * value
+ *
+ * @array: array to be sorted
+ *
+ * @start: lower bound of array (or partition)
+ *
+ * @end: upper bound of array (or partition)
+ *
+ * @size: size of array
+ *
+ * Return: (int) pivot_idx 
+ */
 int Partition(int *array, int start, int end, size_t size)
 {
         int i, pivot_idx, pivot = array[end];
@@ -51,4 +57,54 @@ int Partition(int *array, int start, int end, size_t size)
 	if (array[pivot_idx] != array[end])
 		print_array(array, size);
         return (pivot_idx);
+}
+
+
+/**
+ * sort - Aux function
+ *
+ * Description: This is a function that sorts the array by partitions
+ *
+ * @array: array to be sorted
+ *
+ * @lb: lower bound of array
+ *
+ * @ub: upper bound of array
+ *
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void sort(int *array, int lb, int ub, size_t size)
+{
+	int loc;
+
+	if (lb >= ub || lb < 0)
+		return;
+
+	loc = Partition(array, lb, ub, size);
+	sort(array, lb, loc - 1, size);
+	sort(array, loc + 1, ub, size);
+}
+
+
+/**
+ * quick_sort - Entry point
+ *
+ * Description: This is a function that sorts the array using the Lomuto quick
+ * sort algorithm
+ *
+ * @array: array to be sorted
+ *
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	int lb, ub;
+
+	lb = 0;
+	ub = size - 1;
+	sort(array, lb, ub, size);
 }
