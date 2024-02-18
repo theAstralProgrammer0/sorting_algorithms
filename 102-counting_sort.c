910464f7312737395f7617786819f20b2a9a1358
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-18 06:30:03 +0100

    Added counting sort function

---
+++ 102-counting_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+#define SIZE (size)
+
+void counting_sort(int *array, size_t size)
+{
+	int i, k, *count = NULL, sorted[SIZE];
+
+	k = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > k)
+			k = array[i];
+	}
+
+	count = (int *)calloc((size_t)(k + 1), sizeof(int));
+	if (count == NULL)
+		return;
+
+	for (i = 0; i < (int)size; i++)
+		++count[array[i]];
+	
+	for (i = 1; i <= k; i++)
+		count[i] += count[i - 1];
+
+	for (i = (int)(size - 1); i >= 0; i--)
+		sorted[--count[array[i]]] = array[i];
+
+	print_array(count, k + 1);
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	print_array(array, size);
+
+	free(count);
+}
