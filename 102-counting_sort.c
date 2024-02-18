f4564ea4d2f68ed8bfc63773fa1739a856e73e0c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-18 07:08:47 +0100

    Crazy tinz

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -24,13 +24,12 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < (int)size; i++)
 		++count[array[i]];
 	
-	for (i = 1; i <= k; i++)
+	for (i = 1 ; i <= k; i++)
 		count[i] += count[i - 1];
 
+	print_array(count, k + 1);
 	for (i = (int)(size - 1); i >= 0; i--)
 		sorted[--count[array[i]]] = array[i];
-
-	print_array(count, k + 1);
 
 	for (i = 0; i < (int)size; i++)
 		array[i] = sorted[i];
