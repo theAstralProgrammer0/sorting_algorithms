adcd79d321a5ad6370cf4d28a9d6c5a301f0fcb3
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-24 04:38:15 +0100

    Betty compliance for merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -4,6 +4,18 @@ void copy_array(int *array, int *sorted, int lb, int ub);
 void split(int *array, int *sorted, int lb, int ub);
 void print_sub_array(const int *array, int lb, int ub);
 
+/**
+ * merge_sort - Entry point
+ *
+ * Description: This function sorts an array of unsorted integers using the
+ * merge sort algorithm
+ *
+ * @array: pointer to the array
+ *
+ * @size: size of the array
+ *
+ * Return: Nothing
+ */
 void merge_sort(int *array, size_t size)
 {
 	int lb, ub, *sorted = NULL;
@@ -19,6 +31,22 @@ void merge_sort(int *array, size_t size)
 	free(sorted);
 }
 
+/**
+ * split - Aux function
+ *
+ * Description: This function splits the root array into individual leaves of a
+ * tree data structure
+ *
+ * @array: pointer to the array
+ *
+ * @sorted: pointer to the sorted array
+ *
+ * @lb: lower bound of sub-arrays
+ *
+ * @ub: upper bound of sub-arrays
+ *
+ * Return: Nothing
+ */
 void split(int *array, int *sorted, int lb, int ub)
 {
 	int mid;
@@ -38,8 +66,26 @@ void split(int *array, int *sorted, int lb, int ub)
 		return;
 }
 
-
-void merge( int *array, int *sorted, int lb, int mid, int ub)
+/**
+ * merge - Aux function
+ *
+ * Description: This function merges the sub-arrays in the binary tree
+ * data structure formed by the split funtion and prints each half of
+ * the binary tree
+ *
+ * @array: pointer to the array
+ *
+ * @sorted: pointer to the sorted array
+ *
+ * @lb: lower bound of the sub-arrays
+ *
+ * @mid: mid point of the array
+ *
+ * @ub: upper bound of the sub-arrays
+ *
+ * Return: Nothing
+ */
+void merge(int *array, int *sorted, int lb, int mid, int ub)
 {
 	int i, j, k;
 
@@ -48,7 +94,7 @@ void merge( int *array, int *sorted, int lb, int mid, int ub)
 	k = lb;
 
 	printf("Merging...\n");
-	
+
 	printf("[left]: ");
 	print_sub_array(&array[lb], lb, mid);
 
@@ -88,6 +134,21 @@ void merge( int *array, int *sorted, int lb, int mid, int ub)
 	copy_array(array, sorted, lb, ub);
 }
 
+/**
+ * copy_array - Aux function
+ *
+ * Description: This function copies the elements of one array to another
+ *
+ * @array: pointer to the array
+ *
+ * @sorted: pointer to the sorted array
+ *
+ * @lb: lower bound of array
+ *
+ * @ub: upper bound of array
+ *
+ * Return: Nothing
+ */
 void copy_array(int *array, int *sorted, int lb, int ub)
 {
 	int k;
@@ -98,24 +159,28 @@ void copy_array(int *array, int *sorted, int lb, int ub)
 
 
 /**
- * print_array - Prints an array of integers
+ * print_sub_array - Prints the elements of a sub-array of integers
  *
  * @array: The array to be printed
- * @size: Number of elements in @array
+ *
+ * @lb: lower bound of sub-array
+ *
+ * @ub: upper bound of sub_array
+ *
+ * Return: Nothing
  */
 void print_sub_array(const int *array, int lb, int ub)
 {
-    int i, size;
+	int i, size;
 
-    size = ub - lb + 1;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	size = ub - lb + 1;
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
