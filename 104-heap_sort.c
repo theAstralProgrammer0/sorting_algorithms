9d7a6aa68fc223516373392445926bde0f630077
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-25 05:56:02 +0100

    Added heap algorithm

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,90 +1,127 @@
 #include "sort.h"
 
 /**
- * swap - Aux function
+ * swap - swaps 2 location in a list.
+ * @array: int pointer to the array.
+ * @a: first location of array to be swapped.
+ * @b: second location of array to be swapped.
  *
- * Description: This is a function that swaps two integer values
- *
- * @a: fisrt integer
- *
- * @b: second integer
- *
- * Return: Nothing
+ * Return: void.
  */
-void swap(int *a, int *b)
+void swap(int a, int b, int *array)
 {
 	int temp;
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
 }
 
 /**
- * heapify - Aux function
+ * make_heap - transforms the array into a heap using the
+ * siftdown method.
+ * @array: the array to be transformed.
+ * @size: size of the array.
+ * @idx: index of the parent being checked.
  *
- * Description: This is the function that transforms an array into a max heap
- *
- * @array: pointer to the array
- *
- * @size: size of the arrayy
- *
- * @idx: index of non-leaf node
- *
- * Return: Nothing
+ * Return: void.
  */
-void heapify(int *array, size_t size, int idx)
+void make_heap(int *array, int size, int idx)
 {
-	int l, r, largest;
+	int i = idx, leftchild, rightchild, maxidx;
 
-	largest = idx;
+	leftchild = (i * 2) + 1;
+	rightchild = (i * 2) + 2;
 
-	l = 2 * idx + 1;
-	r = 2 * idx + 2;
+	if (leftchild < (int)size && rightchild < (int)size)
+		maxidx = array[rightchild] > array[leftchild]
+			? rightchild : leftchild; /*if both children are found*/
 
-	if (l < (int)size && array[l] > array[largest])
-		largest = l;
-	if (r < (int)size && array[r] > array[largest])
-		largest = r;
+	else if (leftchild < (int)size && rightchild >= (int)size)
+		maxidx = leftchild; /* if only left child is found */
 
-	if (largest != idx)
+	else			/* base case */
+		return;
+
+	if (array[maxidx] > array[i]) /* swaps parent with biggest child :') */
 	{
-		swap(&array[idx], &array[largest]);
-		printf("Heapify:   ");
+		swap(maxidx, i, array);
 		print_array(array, size);
-		heapify(array, size, largest);
+		make_heap(array, size, maxidx); /* Recursive call */
 	}
 }
 
 /**
- * heap_sort - Entry point
+ * sift_down - re-heapifies an array after deleting an
+ * element from the heap.
+ * @array: the array to be reheapified.
+ * @size: the size of the array.
+ * @idx: the index where we'll stop the heapifying process.
  *
- * Description: This is the function that sorts an array of integers in
- * ascending order using the heap sort (sift-down) algorithm
+ * Return: void.
+ */
+void sift_down(int *array, int size, int idx)
+{
+	int swapflag = 1, parent = 0, rightchild, leftchild;
+
+	while (swapflag)
+	{
+		swapflag = 0;
+		leftchild = (2 * parent) + 1;
+		rightchild = (2 * parent) + 2;
+		if (leftchild < idx && rightchild < idx)
+		{
+			if (array[leftchild] > array[rightchild] &&
+			    array[leftchild] > array[parent])
+			{
+				swap(parent, leftchild, array);
+				print_array(array, size);
+				parent = leftchild;
+				swapflag = 1;
+			}
+			else if (array[rightchild] > array[leftchild] &&
+				 array[rightchild] > array[parent])
+			{
+				swap(parent, rightchild, array);
+				print_array(array, size);
+				parent = rightchild;
+				swapflag = 1;
+			}
+		}
+		else if (leftchild < idx && rightchild >= idx)
+		{
+			if (array[leftchild] > array[parent])
+			{
+				swap(parent, leftchild, array);
+				print_array(array, size);
+				parent = leftchild;
+				swapflag = 1;
+			}
+		}
+	}
+}
+
+/**
+ * heap_sort - sorts an array by creating a heap out of an array
+ * then deleting elements from the heap which leaves a sorted array
+ * behind.
+ * @array: int pointer to the array being sorted.
+ * @size: the size of the array.
  *
- * @array: pointer to the array
- *
- * @size: size of the arrayy
- *
- * Return: Nothing
+ * Return: void.
  */
 void heap_sort(int *array, size_t size)
 {
-	int i, nln;
+	int i;
 
-	if (array == NULL || size == 1)
+	if (array == NULL || size < 2)
 		return;
-
-	nln = (size / 2) - 1;
-
-	for (i = nln; i >= 0; i--)
-		heapify(array, size, i);
-
-	for (i = size - 1; i >= 0; i--)
+	for (i = (int)size - 1; i >= 0; i--)
+		make_heap(array, size, i);
+	for (i = (int)size - 1; i > 0; i--)
 	{
-		swap(&array[0], &array[i]);
-		printf("Heap Sort: ");
+		swap(i, 0, array);
 		print_array(array, size);
-		heapify(array, size, 0);
+		sift_down(array, size, i);
 	}
 }
